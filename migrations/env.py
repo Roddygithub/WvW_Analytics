74afafb9405bc2a9c5737f26c05e4027570650d8
Roland Salardon <roddy@desktop-roddy.home>
2025-12-24 00:16:00 +0100

    Widen alembic version column type to String(64)

--- migrations/env.py
+++ migrations/env.py
@@ -56,6 +56,9 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        version_table="alembic_version",
+        version_column="version_num",
+        version_table_column_type=String(64),
     )
 
     with context.begin_transaction():
@@ -77,7 +80,11 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            version_table="alembic_version",
+            version_column="version_num",
+            version_table_column_type=String(64),
         )
 
         with context.begin_transaction():
